11d21895794459750baaf6f0a9d88ef79ba68219
Pedro <Pedroxxdroxx@gmail.com>
2025-09-14 21:46:09 -0300

    commit-hello-word

---
+++ Hello/test_lambda_function.py
@@ -0,0 +1,17 @@
+import unittest
+import json
+from lambda_function import lambda_handler
+
+class TestLambdaFunction(unittest.TestCase):
+    def test_lambda_handler_returns_hello_world(self):
+        # Simulando evento vazio (sem dados de entrada)
+        event = {}
+        context = None
+
+        response = lambda_handler(event, context)
+
+        self.assertEqual(response['statusCode'], 200)
+        self.assertEqual(json.loads(response['body']), 'Hello World!')
+
+if __name__ == '__main__':
+    unittest.main()
